b3231dd0f7dc1d2c1f73a70cda6c571811ff45e9
Vijay <teriyatha.napar@gmail.com>
2023-03-14 15:18:12 +0800

    Day 5, Part 1 complete

---
+++ 2022/README.md
@@ -0,0 +1,14 @@
+## About
+
+This **WIP** repo contains solutions to challenges posted as a part of (**Advent of code**)[https://adventofcode.com/] using (**Go programming language**)[https://go.dev/].
+
+## Try it out
+
+Simple `cd` into the folder to read the requirements and run the following command for the answers:
+
+```
+cd day1
+go run main.go
+```
+
+#Happy Coding!
---
+++ 2022/day5/main.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Read data from data file
+func ReadDataFromFile(filepath string) []string {
+	file, err := os.Open(filepath)
+
+	if err != nil {
+		fmt.Println("Err")
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var fileContent = []string{}
+
+	for scanner.Scan() {
+
+		rowInFile := scanner.Text()
+
+		fileContent = append(fileContent, rowInFile)
+
+	}
+
+	return fileContent
+}
+
+/*
+	Data strcuture design
+
+	Stack data
+	A map might be suitable. The keys will represent stack.
+	The values will be a string based slice representing the crates in a stack
+
+
+	Move instructions data
+	This will be represented using multi dimentional slices.
+	Each row from raw data will be an entry in the slice.
+	The move steps in a single row will be represented as an element of a slice after being parsed
+
+*/
+func buildStack(rawData []string) map[int][]string {
+
+	var stackData = make(map[int][]string)
+
+	for i := len(rawData) - 1; i >= 0; i-- {
+
+		var stack int
+		row := rawData[i]
+
+		for j := 0; j <= len(row); j = j + 4 {
+
+			/*
+				if j == len(row) {
+					fmt.Println("Break: ", j)
+					break
+				}
+			*/
+
+			//fmt.Println(j, row[j:j+3])
+
+			stack++
+			cellEntry := strings.ReplaceAll(row[j:j+3], " ", "")
+			if cellEntry == "" {
+				//stackData[stack] = append(stackData[stack], "")
+			} else {
+				//cellEntry = strings.ReplaceAll(row[j:j+3], "[", "")
+				//stackData[stack] = strings.ReplaceAll(cellEntry, "]", ""))
+				stackData[stack] = append(stackData[stack], row[j:j+3])
+			}
+
+			//stackData[stack] = reverseSlice(stackData[stack])
+
+		}
+
+	}
+
+	return stackData
+
+}
+
+/*
+	Function to prepare moves data and normalize it into an array
+*/
+func prepareMoveSteps(rawData []string) [][]int {
+
+	var allMoves [][]int
+
+	for _, row := range rawData {
+
+		splitAction := strings.Split(row, " ")
+		//fmt.Println(splitAction[1], splitAction[3], splitAction[5])
+
+		crateCount, _ := strconv.Atoi(splitAction[1])
+		fromStack, _ := strconv.Atoi(splitAction[3])
+		toStack, _ := strconv.Atoi(splitAction[5])
+
+		move := []int{crateCount, fromStack, toStack}
+
+		allMoves = append(allMoves, move)
+
+	}
+
+	return allMoves
+
+}
+
+/*
+Function to make the actual moves and rearrange stacks as per move data
+*/
+func makeMove(actions [][]int, stackData map[int][]string) {
+
+	for _, action := range actions {
+
+		//fmt.Println("Action >", action)
+
+		toSlice := stackData[action[2]]
+		fromSlice := stackData[action[1]]
+
+		for j := action[0]; j > 0; j-- {
+
+			toSlice = append(toSlice, fromSlice[len(fromSlice)-1])
+			fromSlice = stackData[action[1]][:len(fromSlice)-1]
+
+		}
+
+		stackData[action[1]] = fromSlice
+		stackData[action[2]] = toSlice
+
+	}
+
+	for k := 1; k <= len(stackData); k++ {
+		fmt.Println(k, stackData[k])
+	}
+
+}
+
+func processRawData(rawData []string) {
+
+}
+
+func main() {
+
+	//TestData
+	/*
+		rawData := ReadDataFromFile("./input/data.txt")
+		stackData := buildStack(rawData[:3])
+		allMoves := prepareMoveSteps(rawData[5:9])
+	*/
+
+	rawData := ReadDataFromFile("./input/data_large.txt")
+	stackData := buildStack(rawData[:8])
+	allMoves := prepareMoveSteps(rawData[10:])
+
+	makeMove(allMoves, stackData)
+
+}
